ae0cf0f8627ea09f45f22948ca7a6394d62c3ecf
phanta864 <ephantusmwangi864@gmail.com>
2018-10-12 12:04:58 +0300

    Write test for getName(), getAge() in ClientTest and write corresponding functions in Clent.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,18 @@
 public class Client {
-    public Client(String name, int age, String firstAppearance, String neighbourhood){
+    private String mName;
+    private int mAge;
+    private String mFirstAppearance;
+    private String mNeighbourhood;
 
+
+    public Client(String name, int age, String firstAppearance, String neighbourhood){
+        mName =name;
+        mAge = age;
+        mFirstAppearance = firstAppearance;
+        mNeighbourhood = neighbourhood;
+    }
+
+    public String getName(){
+        return mName;
     }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -8,4 +8,9 @@ public class ClientTest {
         Client client = new Client("shiro", 20, "may", "koma");
         assertTrue(client instanceof Object);
     }
+    @Test
+    public void getNameOfClient_String(){
+        Client client = new Client("shiro", 20, "may", "koma");
+        assertEquals("shiro", client.getName());
+    }
 }
